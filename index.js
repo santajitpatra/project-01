b832a100035c9d66a090847faffd548411b7cbf0
Santajit Patra <business.santajitpatra@gmail.com>
2023-07-06 20:07:35 +0530

    http status code
    http status code and their usage
    if 400 error go and fix

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ const port = 8000;
 // app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  fs.appendFile("log.txt", `${Date.now()}: ${req.ip}: ${req.method}: ${req.path}\n`, (err, data) => {
-    next();
-  });
+  fs.appendFile(
+    "log.txt",
+    `${Date.now()}: ${req.ip}: ${req.method}: ${req.path}\n`,
+    (err, data) => {
+      next();
+    }
+  );
   // console.log("first request from middleware");
-  req.myUserName = "test"
+  req.myUserName = "test";
 });
 
 // app.use((req, res, next) => {
@@ -22,8 +26,8 @@ app.use((req, res, next) => {
 
 // REST API
 app.get("/api/users", (req, res) => {
-  res.setHeader('X-MyName', 'john doe');
-    // console.log("first route 2", req.myUserName);
+  res.setHeader("X-MyName", "john doe");
+  // console.log("first route 2", req.myUserName);
   res.json(users);
 });
 
@@ -39,6 +43,7 @@ app
   .get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
+    if (!user) return res.status(404).json({ message: "error" });
     return res.json(user);
   })
   .put((req, res) => {
@@ -56,10 +61,19 @@ app
 
 app.post("/api/users", (req, res) => {
   const body = req.body;
+  if (!body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  ) {
+    return res.status(400).json({ message: "Please provide all fields" });
+  }
   // console.log("body: ", body)
   users.push({ ...body, id: users.length + 1 });
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.json({ statusbar: "pending", id: users.length });
+    return res.status(201).json({ statusbar: "pending", id: users.length });
   });
 });
 
